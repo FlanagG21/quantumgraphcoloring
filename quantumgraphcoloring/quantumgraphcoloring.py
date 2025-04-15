e9f6564e8477580c3635b43557b662a963e54455
Galen Flanagan <flanagg21@vt.edu>
2025-04-15 16:10:23 +0000

    started edge representation

--- quantumgraphcoloring/quantumgraphcoloring.py
+++ quantumgraphcoloring/quantumgraphcoloring.py
@@ -1,5 +1,21 @@
 """Provide the primary functions."""
+from .bitstring import *
+import numpy as np
+class coloring:
+    def __init__(self, K, G = [[]]):
+        """_summary_
 
-
+        Args:
+            K (_type_): Integer, number of colors
+            G (list, optional): Adjacency matrix for nodes
+        """
+        self.K = K
+        self.G = G
+        self.Edges = []
+        for i in range(len(self.J)):
+            nodes = len(self.J[i])
+            self.Edges.append(np.zeros(nodes, dtype=int))
+            for j in range(i+1, len(self.J[i])):
+        
 
 
